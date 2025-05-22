3dffb697827e415e867e21599f5cb73d9a0ffb8a
Nick Killeen <nick@aleaquo.com>
2025-05-22 09:14:16 -0500

    Implemented the methods to create game instances

--- src/mafia_backend/Database/query_service.py
+++ src/mafia_backend/Database/query_service.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
-from db_connection import Database
+from sqlalchemy.orm import sessionmaker
+
+from .db_connection import Database
 from .db_models import *
 
 from mafia_backend.payload_models import GamePayload
@@ -8,6 +10,21 @@ from mafia_backend.payload_models import GamePayload
 class QueryService(Database):
     def __init__(self):
         super().__init__()
+        self.engine  = self.get_engine()
+        self.Session = sessionmaker(bind=self.engine)
+
+    def _get_winning_team_id(self, winning_team):
+        """
+        Given a winning team name, return the corresponding DB ID
+        """
+
+        with self.Session() as session:
+            team: Team = session.query(Team).filter(Team.name == winning_team).first()
+            if team is None:
+                raise ValueError(f"No team found with name {winning_team}")
+            return team.team_id
+
+
 
     def create_game(self, game_payload: GamePayload):
         played_at = datetime.now()
@@ -18,18 +35,20 @@ class QueryService(Database):
             if player.isMafia:
                 mafia_count += 1
 
+        winning_team_id = self._get_winning_team_id(game_payload.winningTeam)
 
         with self.Session() as session:
             game = Game(
                 played_at = played_at,
-                winning_team = game_payload.winningTeam,
+                winning_team = winning_team_id,
                 player_count = player_count,
                 mafia_count = mafia_count,
-                mafia_kill_power = game_payload.mafiaKillPower,
+                mafia_kill_power = game_payload.initialMafiaKillPower,
                 day_count = game_payload.dayCount,
                 night_count = game_payload.nightCount
             )
             session.add(game)
             session.commit()
+            return game.game_id
             
 
