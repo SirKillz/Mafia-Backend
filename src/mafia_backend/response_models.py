e5e4dd79c978990e4161c5bd48b97c133e027c16
Nick Killeen <nick@aleaquo.com>
2025-05-23 16:03:02 -0500

    Created the response model for player stats

--- src/mafia_backend/response_models.py
+++ src/mafia_backend/response_models.py
@@ -8,3 +8,21 @@ class PlayerSchema(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+class PlayerStatsSchema(BaseModel):
+    player_id: int
+    player_name: str
+    games_played: int
+    mafia_games: int
+    innocent_games: int
+    mafia_win_rate: float | None
+    innocent_win_rate: float | None
+    spy_check_rate: float | None
+    medic_self_save_rate: float | None
+    successful_medic_save_rate: float | None
+    successful_assassin_shot_rate: float | None
+
+    model_config = {
+        "from_attributes": True
+    }
