51f82e1c60ebb000d16ac0c4a86b4a279a8845ad
Nick Killeen <sirkillz1198@gmail.com>
2025-05-23 10:24:39 -0500

    Updated response model for receiving players

--- src/mafia_backend/response_models.py
+++ src/mafia_backend/response_models.py
@@ -1,8 +1,10 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 class PlayerSchema(BaseModel):
     player_id: int
-    player_name: str
+    name:      Optional[str] = None
 
     class Config:
         orm_mode = True
