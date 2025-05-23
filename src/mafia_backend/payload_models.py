087bab49e8c7adbb63f6db7c43f81e9ad844af52
Nick Killeen <sirkillz1198@gmail.com>
2025-05-23 11:17:33 -0500

    Updated the game player schema to check for the individual player stats

--- src/mafia_backend/payload_models.py
+++ src/mafia_backend/payload_models.py
@@ -13,6 +13,64 @@ class PlayerSchema(BaseModel):
     isMafia: bool
     isSpecialInnocent: bool
     isSpecialMafia: bool
+    spyMafiaHitPercentage: list
+    medicSavePercentage: list
+    medicSelfSavePercentage: list
+    assassinMafiaHitPercentage: list
+
+    def spy_stats(self):
+        positive_actions = 0
+        total_opps = len(self.spyMafiaHitPercentage)
+
+        for action in self.spyMafiaHitPercentage:
+            if action:
+                positive_actions += 1
+        
+        return {
+            "numerator": positive_actions,
+            "denominator": total_opps
+        }
+    
+    def medic_save_stats(self):
+        positive_actions = 0
+        total_opps = len(self.medicSavePercentage)
+
+        for action in self.medicSavePercentage:
+            if action:
+                positive_actions += 1
+        
+        return {
+            "numerator": positive_actions,
+            "denominator": total_opps
+        }
+    
+    def medic_self_save_stats(self):
+        positive_actions = 0
+        total_opps = len(self.medicSelfSavePercentage)
+
+        for action in self.medicSelfSavePercentage:
+            if action:
+                positive_actions += 1
+        
+        return {
+            "numerator": positive_actions,
+            "denominator": total_opps
+        }
+    
+    def assassin_stats(self):
+        positive_actions = 0
+        total_opps = len(self.assassinMafiaHitPercentage)
+
+        for action in self.assassinMafiaHitPercentage:
+            if action:
+                positive_actions += 1
+        
+        return {
+            "numerator": positive_actions,
+            "denominator": total_opps
+        }
+
+
 
 class GamePayload(BaseModel):
     players: List[PlayerSchema]
